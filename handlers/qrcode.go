941b2cf2090e549c91b7680c4ff3700cdfaa0da6
Daniel Cunha (soro) <danielsoro@gmail.com>
2017-06-07 15:53:15 -0300

    Move endpoint to accept only JSON

--- handlers/qrcode.go
+++ handlers/qrcode.go
@@ -6,20 +6,27 @@ import (
 	"github.com/skip2/go-qrcode"
 )
 
+// QRCodeRequest is a struct that represent the data request by client
+type QRCodeRequest struct {
+	URL string `form:"url" json:"url" binding:"required"`
+}
+
 // QrcodeHandler is a handler function to generate the QRCode
 func QrcodeHandler(context *gin.Context) {
-	url := context.PostForm("url")
-	qrcode, error := generateQrCode(url)
-	if error != nil {
-		panic(error)
-	}
+	var json QRCodeRequest
+	if context.BindJSON(&json) == nil {
+		qrcode, error := generateQrCode(json.URL)
+		if error != nil {
+			context.AbortWithError(400, error)
+		}
 
-	bytes, error := qrcode.PNG(256)
-	if error != nil {
-		panic(error)
+		bytes, error := qrcode.PNG(256)
+		if error != nil {
+			context.AbortWithError(500, error)
+		}
+		context.Render(200, render.Data{ContentType: "image/png", Data: bytes})
 	}
-
-	context.Render(200, render.Data{ContentType: "image/png", Data: bytes})
+	context.AbortWithStatus(400)
 }
 
 func generateQrCode(url string) (*qrcode.QRCode, error) {
