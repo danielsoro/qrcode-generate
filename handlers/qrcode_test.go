acff6752756d7bef9c964d45721380b5083c5a24
Daniel Cunha <dcunha@tomitribe.com>
2017-06-23 13:36:13 -0300

    Improvement on tests

--- handlers/qrcode_test.go
+++ handlers/qrcode_test.go
@@ -3,6 +3,7 @@ package handlers_test
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -26,11 +27,12 @@ func TestShouldReturnAByteArray(t *testing.T) {
 	r.ServeHTTP(respRecorder, req)
 
 	if respRecorder.Code != http.StatusOK {
+		t.Log(fmt.Sprintf("HTTP %v expected, but received %v", http.StatusOK, respRecorder.Code))
 		t.Fail()
 	}
 
 	if respRecorder.Body == nil {
-		t.Log("POST /qrcode not returned body")
+		t.Log("POST /qrcode wasn't return body")
 		t.Fail()
 	}
 }
